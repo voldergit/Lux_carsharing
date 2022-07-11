c31c24f81b60c95b0b72906fe28656930eb459a2
voldergit <voldergit@gmail.com>
2022-07-11 21:05:48 +0300

    section feedback + footer, adaptive

--- js/main.js
+++ js/main.js
@@ -4,11 +4,44 @@ $(function () {
     dots: true,
     autoplay: true,
     fade: true,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          dots: false,
+        },
+      },
+    ],
   });
+
   $(".reviews_slider").slick({
     arrows: false,
     dots: true,
     slidesToShow: 4,
     slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1140,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 845,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 585,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
+
+  $(".menu_btn").on("click", function () {
+    $(".menu_list").toggleClass("menu_list--active");
   });
 });
